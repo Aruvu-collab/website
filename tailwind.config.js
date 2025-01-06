3e5f3ff92c7d46b61c363aef002bf8117b72bb52
adhavan <adhavan@aruvu.org>
2025-01-06 20:58:55 +0530

    fix: go to adapter cloudflare to attempt to avoid unnecessary routes.json addition

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
-module.exports = {
+const config = {
   content: [
-    './src/**/*.{html,js,svelte,ts}', // Ensure your Svelte files are included here
+    "./src/**/*.{html,js,svelte,ts}", // Ensure your Svelte files are included here
   ],
   theme: {
     extend: {
       colors: {
-        aruvupink: '#FF94AC',
-        aruvulightpink: '#FDD0DA',
+        aruvupink: "#FF94AC",
+        aruvulightpink: "#FDD0DA",
         brown: {
-          50: '#f5f5f4',
-          200: '#c6a58d',
-        }
+          50: "#f5f5f4",
+          200: "#c6a58d",
+        },
       },
       fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-      }
+        sans: ["Inter", "sans-serif"],
+      },
     },
   },
   plugins: [],
