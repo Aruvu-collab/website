2703e9700116065a87c8b08899a74a6980116076
adhavan <adhavan@aruvu.org>
2025-02-13 17:36:05 +0530

    Create contact page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,23 @@
+<div class="card-container">
+	<!-- Image -->
+	<div class="h-[60vh] w-full">
+		<figure>
+			<img src="aruvu_ync.webp" class="h-auto w-full object-cover grayscale" />
+			<caption class="w-full p-3 text-xs text-gray-500"
+				>Picture, from left to right: At the Yelahanka Nodal Center, Eshwari drinks tea, Vinay
+				attends a call, Supriya is at her notes, while Shreyas, Naveen and Harsha are conversing.
+			</caption>
+		</figure>
+	</div>
+
+	<!-- Text Content -->
+	<div class=" p-6">
+		<h3 class="text-left text-xl font-semibold">You can see us here</h3>
+		Yelahanka Nodal Center, Aruvu Collaboratory<br />
+		226, SFS 407, KHB Colony 4th Phase,<br />
+		Yelahanka New Town, Bengaluru - 560064
+
+		<h3 class="text-left text-xl font-semibold">Or email at</h3>
+		wellbeing@aruvu.org
+	</div>
+</div>
